c754dc6a7d65da2ad1fcb9b471b131fb133e0877
bigyihsuan <bigyihsuan@gmail.com>
2024-05-08 11:24:06 -0400

    finish refactoring

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -99,12 +99,10 @@ impl Evaluator {
     }
 
     pub fn set_alias(&mut self, name: &str, func: &Box<Node>) -> Option<Obj> {
-        self.aliases
-            .insert(name.to_owned(), func.clone())
-            .map(|_| Obj::Alias {
-                name: name.to_owned(),
-                func: func.clone(),
-            })
+        self.aliases.insert(name.to_owned(), func.clone()).map(|_| Obj::Alias {
+            name: name.to_owned(),
+            func: func.clone(),
+        })
     }
 
     pub fn next_node(&mut self) -> Option<Node> {
@@ -121,79 +119,87 @@ pub trait Eval {
 }
 pub trait EvalNodes {
     fn node(&mut self, node: &Node) -> Option<Obj>;
+    fn call(&mut self, name: &str) -> Option<Obj>;
     fn obj(&mut self, obj: &Obj) -> Option<Obj>;
+    fn r#if(&mut self, cond: &Node, when_true: &Node, when_false: &Option<Box<Node>>) -> Option<Obj>;
+    fn alias(&mut self, name: &str, func: &Box<Node>) -> Option<Obj>;
 }
 
 impl EvalNodes for Evaluator {
     fn node(&mut self, node: &Node) -> Option<Obj> {
         match node {
-            Node::Call(name) => {
-                println!("evaling call {name}");
-                let f = self.get_builtin(name);
-                if let Some(Obj::BuiltinFunc(f)) = f {
-                    println!("    calling builtin {}", name.clone());
-                    println!(
-                        "    dequeuing {} {}",
-                        f.arity,
-                        if f.arity == 1 { "node" } else { "nodes" }
-                    );
-                    for _ in 0..f.arity {
-                        let out = self.next_node();
-                        if let Some(out) = out {
-                            let out = self.node(&out)?;
-                            self.push_value(out)
-                        }
-                    }
-                    return (f.code)(self);
-                }
-
-                let f = self.get_alias(name);
-                if let Some(f) = f {
-                    println!("    calling alias {}", name.clone());
-                    return self.node(&f);
-                }
-                None
-            }
-            Node::Obj(o) => {
-                println!("evaling object");
-                self.obj(&o)
-            }
+            Node::Call(name) => self.call(name),
+            Node::Obj(o) => self.obj(&o),
             Node::If {
                 cond,
                 when_true,
                 when_false,
-            } => {
-                println!("evaling if");
-                let cond = self.node(cond);
-                if let Some(cond) = cond {
-                    let cond = cond.into();
-                    if cond {
-                        println!("    evaling when_true");
-                        self.node(when_true)
-                    } else if let Some(when_false) = &when_false {
-                        println!("    evaling when_false");
-                        self.node(when_false)
-                    } else {
-                        None
-                    }
-                } else {
-                    None
-                }
-            }
-            Node::Alias { name, func } => {
-                println!("evaling alias");
-                self.set_alias(name, func)
-            } // node => {
-              //     println!("TODO: unknown node: {node:?}");
-              //     None
-              // }
+            } => self.r#if(cond, when_true, when_false),
+            Node::Alias { name, func } => self.alias(name, func),
+            // node => {
+            //     println!("TODO: unknown node: {node:?}");
+            //     None
+            // }
         }
     }
 
+    fn call(&mut self, name: &str) -> Option<Obj> {
+        println!("evaling call {name}");
+        let f = self.get_builtin(name);
+        if let Some(Obj::BuiltinFunc(f)) = f {
+            println!("    calling builtin {}", name);
+            println!(
+                "    dequeuing {} {}",
+                f.arity,
+                if f.arity == 1 { "node" } else { "nodes" }
+            );
+            for _ in 0..f.arity {
+                let out = self.next_node();
+                if let Some(out) = out {
+                    let out = self.node(&out)?;
+                    self.push_value(out)
+                }
+            }
+            return (f.code)(self);
+        }
+
+        let f = self.get_alias(name);
+        if let Some(f) = f {
+            println!("    calling alias {}", name);
+            return self.node(&f);
+        }
+        None
+    }
+
     fn obj(&mut self, obj: &Obj) -> Option<Obj> {
+        println!("evaling object");
         match obj {
             Obj::BuiltinFunc(f) => (f.code)(self),
             _ => Some(obj.clone()),
         }
+    }
+
+    fn r#if(&mut self, cond: &Node, when_true: &Node, when_false: &Option<Box<Node>>) -> Option<Obj> {
+        println!("evaling if");
+        let cond = self.node(cond);
+        if let Some(cond) = cond {
+            let cond = cond.into();
+            if cond {
+                println!("    evaling when_true");
+                self.node(when_true)
+            } else if let Some(when_false) = &when_false {
+                println!("    evaling when_false");
+                self.node(when_false)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    fn alias(&mut self, name: &str, func: &Box<Node>) -> Option<Obj> {
+        println!("evaling alias");
+        self.set_alias(name, func)
     }
 }
