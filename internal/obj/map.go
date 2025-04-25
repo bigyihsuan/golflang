abeaa8102e79241879e0c9053ae5599abdf9ae43
bigyihsuan <bigyihsuan@gmail.com>
2025-04-25 09:13:10 -0400

    move back to map impl, sort before stringifying

--- internal/obj/map.go
+++ internal/obj/map.go
@@ -7,24 +7,14 @@ import (
 	"strings"
 )
 
-type Map struct {
-	m    map[Obj]Obj
-	keys []Obj
-}
+type Map map[Obj]Obj
 
 func ZeroMap() Map {
-	return Map{m: make(map[Obj]Obj), keys: []Obj{}}
+	return Map(make(map[Obj]Obj))
 }
 
 func MapFromMap(m map[Obj]Obj) Map {
-	out := ZeroMap()
-	out.m = m
-	keys := slices.Collect(maps.Keys(m))
-	if keys == nil {
-		keys = []Obj{}
-	}
-	out.keys = keys
-	return out
+	return Map(m)
 }
 
 func MapFromPairs(kvs ...Obj) Map {
@@ -35,21 +25,21 @@ func MapFromPairs(kvs ...Obj) Map {
 	pairs := slices.Chunk(kvs, 2)
 	for pair := range pairs {
 		k, v := pair[0], pair[1]
-		out.Add(k, v)
+		out[k] = v
 	}
 	return out
 }
 
 // Bool implements Obj.
 func (m Map) Bool() bool {
-	return len(m.m) > 0
+	return len(m) > 0
 }
 
 // Equal implements Obj.
 func (m Map) Equal(o Obj) bool {
 	switch o.Kind() {
 	case KindMap:
-		return maps.EqualFunc(m.m, o.(Map).m, func(l, r Obj) bool { return l.Equal(r) })
+		return maps.EqualFunc(m, o.(Map), func(l, r Obj) bool { return l.Equal(r) })
 	default:
 		return false
 	}
@@ -63,46 +53,19 @@ func (m Map) Kind() ObjKind {
 // Repr implements Obj.
 func (m Map) Repr() string {
 	vs := []string{}
-	for _, k := range m.keys {
-		v := m.m[k]
+	for k, v := range m {
 		vs = append(vs, fmt.Sprintf("%s:%s", k.Repr(), v.Repr()))
 	}
+	slices.Sort(vs)
 	return fmt.Sprintf("{%s}", strings.Join(vs, ","))
 }
 
 // String implements Obj.
 func (m Map) String() string {
 	vs := []string{}
-	for _, k := range m.keys {
-		v := m.m[k]
+	for k, v := range m {
 		vs = append(vs, fmt.Sprintf("%s:%s", k.String(), v.String()))
 	}
+	slices.Sort(vs)
 	return fmt.Sprintf("{%s}", strings.Join(vs, ","))
-}
-
-// replicates m[k] = v
-func (m *Map) Add(k, v Obj) {
-	m.m[k] = v
-	if !slices.ContainsFunc(m.keys, func(o Obj) bool { return o.Equal(k) }) {
-		m.keys = append(m.keys, k)
-	}
-}
-
-// replicates v,ok := m[k]
-func (m *Map) Get(k Obj) (Obj, bool) {
-	v, ok := m.m[k]
-	return v, ok
-}
-
-// replicates delete(m,k)
-func (m *Map) Delete(k Obj) {
-	_, ok := m.Get(k)
-	if ok {
-		delete(m.m, k)
-		i := slices.IndexFunc(m.keys, func(o Obj) bool { return o.Equal(k) })
-		if i < 0 {
-			return
-		}
-		m.keys = slices.Delete(m.keys, i, i+1)
-	}
 }
